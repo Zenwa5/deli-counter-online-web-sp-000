47d27caa22ccbd987c92fe8ad79da10b66de391e
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-14 23:10:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,5 @@
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  elsif katz_deli.length > 0
-    puts "The line is currently: #{katz_deli.length}""
   end
 end
