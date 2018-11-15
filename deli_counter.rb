0f7b5be303ab1e4e3a67a060c08274e766c917ee
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-15 00:03:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif katz_deli.length > 0
     string = "The line is currently: "
-    katz_deli.each do |customer|
-      string << "#{katz_deli.index(customer) + 1 }. #{customer}"
+    katz_deli.each  {|customer|string << "#{katz_deli.index(customer) + 1 }. #{customer}"}
     puts string
 
   end
