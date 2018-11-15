0193b46259f5e9f1b017cf5a601b5625c8a95dce
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-15 00:30:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,9 +16,12 @@ def take_a_number(katz_deli, person)
 end
 
 def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  end
  while katz_deli.length > 0
    puts "Currently serving #{katz_deli[0]}."
    katz_deli.shift
 end
-puts "There is nobody waiting to be served!"
+
 end
